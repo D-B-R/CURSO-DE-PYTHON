5e1c34df0ccfd7e9a3476334afd14436cba3e526
Daniel Bernardo Rocha <33178580+danielbernardouenp@users.noreply.github.com>
2019-11-14 14:20:57 -0300

    Update Aula5.py

--- Aula5.py
+++ Aula5.py
@@ -1,6 +1,6 @@
 #crie um programa que leia quanto dinheiro uma pessoa tem
 # na carteira mostre quantos dolares ela pode comprar
-#considere U$$ 1 = R$3,25
+#considere U$$ 1 = R$3,27
 
 n1=float(input('Digite o valor que tem na carteira\n'))
 n2=n1/3.27
