329cde87cf0967cae629a448feedf9f0bde7be6b
Daniel Bernardo Rocha <33178580+danielbernardouenp@users.noreply.github.com>
2019-11-14 13:42:06 -0300

    Update Aula2.py

--- Aula2.py
+++ Aula2.py
@@ -5,4 +5,4 @@
 n1= int(input('Digete um número!\n'))
 n2=n1-1
 n3=n1+2
-print('O antecessor do número  {} é {} e o sucessor do numero {} é {}'.format(n1,n2,n1,n3))
+print('O antecessor do número  {} é {} e o sucessor do número {} é {}'.format(n1,n2,n1,n3))
