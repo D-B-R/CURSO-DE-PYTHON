00c00c1b00ba20450d86ff1d567c18bb58cd5071
Daniel Bernardo Rocha <33178580+danielbernardouenp@users.noreply.github.com>
2019-11-14 13:39:23 -0300

    Add files via upload

---
+++ Aula2.py
@@ -0,0 +1,8 @@
+#faça um programa que leia um número inteiro e mostre o seu sucessor
+#e seu antecessor
+
+
+n1= int(input('Digete um número!\n'))
+n2=n1-1
+n3=n1+2
+print('O antecessor do número  {} é {} e o sucessor do numero {} é {}'.format(n1,n2,n1,n3))
