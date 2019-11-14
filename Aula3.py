1a0dfc41b040935d7ca263de0a2f31396f30226d
Daniel Bernardo Rocha <33178580+danielbernardouenp@users.noreply.github.com>
2019-11-14 14:01:42 -0300

    Add files via upload

---
+++ Aula3.py
@@ -0,0 +1,9 @@
+#crie um algoritmo que leia duas notas calcule e mostre sua media
+
+
+n1= float(input('Digite a nota da primeira prova!\n'))
+n2=float(input('Digite a nota da segunda prova!\n'))
+n3=n1+n2
+n4= n3/2
+
+print('a soma das notas das prova são de {} e a media é de {:.2f}'.format(n3,n4))
